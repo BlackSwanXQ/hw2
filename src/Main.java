e95dadd7f229126cd4388e8538a468f80de963e3
Алексей Шлапаков <theblack2swan@gmail.com>
2024-01-15 19:52:19 +0300

    My hw2

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+        // task2
+        dog = dog+4;
+        System.out.println(dog);
+        cat = cat+4;
+        System.out.println(cat);
+        paper = paper +4;
+        System.out.println(paper);
+
+        //task3
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+
+        //task4
+        var friend = 19;
+        System.out.println(friend);
+        friend= friend +2;
+        System.out.println(friend);
+        friend = friend/7;
+        System.out.println(friend);
+
+        //task5
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog*10;
+        System.out.println(frog);
+        frog = frog/3.5;
+        System.out.println(frog);
+        frog = frog +4;
+        System.out.println(frog);
+
+        //task6
+        var boxerWeightOne = 78.2;
+        var boxerWeightTwo = 82.7;
+        var totalWeight = boxerWeightOne + boxerWeightTwo;
+        System.out.println(totalWeight);
+        var differWeight = boxerWeightOne - boxerWeightTwo;
+        System.out.println(differWeight);
+
+        //task7
+        var differWeight2 = boxerWeightTwo - boxerWeightOne;
+        System.out.println(differWeight2);
+        var differWeight3 = boxerWeightTwo % boxerWeightOne;
+        System.out.println(differWeight3);
+
+        //task8
+        var allTime = 640;
+        var everyTime = 8;
+        var emploeeCount = allTime/everyTime;
+        System.out.println("Всего работников в компании - "+ emploeeCount + " человек");
+
+        var emploeeCount2 = emploeeCount + 94;
+        System.out.println(emploeeCount2);
+        var everyTime2 = 640/emploeeCount2;
+        System.out.println("Если в компании работает " + emploeeCount2 + " человека, то всего " + everyTime2 + " часа работы может быть поделено между сотрудниками");
+
+
+    }
+}
